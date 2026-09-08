1fad7905aba3ac3544d0fb810a7dc5da71036237
soham855 <139761026+soham855@users.noreply.github.com>
2026-09-08 12:22:14 +0530

    Set Instant Services app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.uniquemarket.service',
-  appName: 'Unique Market',
+  appId: 'com.uniquemarket.instantservices',
+  appName: 'Instant Services for Your Security',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
